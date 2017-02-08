de4331b565529f279255d36c9792328a3580134b
Daniel.Sendula <daniel.sendula@creative-sphere.com>
2017-02-08 14:04:59 +0000

    Added radar client and agent

---
+++ client/playground/radar-agent.py
@@ -0,0 +1,119 @@
+import RPi.GPIO as GPIO
+import time
+import traceback
+import random
+import sys
+import paho.mqtt.client as mqtt
+
+
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 11  # 23
+ECHO = 8   # 24
+
+SERVO_NUMBER = 8
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.output(TRIG, False)
+
+print("Waiting for sensor to settle")
+
+time.sleep(1)
+
+connected = False
+
+client = mqtt.Client("radar-agent-#" + str(random.randint(1000, 9999)))
+
+def moveServo(angle):
+    # angle is between -90 and 90
+    angle += 150
+    angle = int(angle)
+
+    f = open("/dev/servoblaster", 'w')
+    f.write(str(SERVO_NUMBER) + "=" + str(angle) + "\n")
+    f.close()
+
+
+def readDistance():
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    start = time.time()
+    while GPIO.input(ECHO) == 0 and time.time() - start < 0.1:
+        pass
+
+    pulse_start = time.time()
+
+    while GPIO.input(ECHO) == 1 and time.time() - start < 0.3:
+        pass
+
+    pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 171500
+
+    distance = round(distance, 2)
+
+    return distance
+
+
+def onConnect(mqttClient, data, rc):
+    global connected
+    try:
+        if rc == 0:
+            connected = True
+
+            #
+            # Subscribe to the topic goes here
+            #
+
+        else:
+            print("ERROR: Connection returned error result: " + str(rc))
+            sys.exit(rc)
+
+    except Exception as ex:
+        print("ERROR: Got exception on connect; " + str(ex))
+
+
+def onMessage(mqttClient, data, msg):
+    global wheelsStopped
+
+    try:
+        payload = str(msg.payload, 'utf-8')
+        topic = msg.topic
+
+        #
+        # Reading distance and sending data back (publish) goes here
+        #
+
+    except Exception as ex:
+        print("ERROR: Got exception on message; " + str(ex))
+
+
+try:
+    print("Connecting to the broker.")
+
+    client.on_connect = onConnect
+    client.on_message = onMessage
+
+    client.connect("localhost", 1883, 60)
+
+    while not connected:
+        client.loop()
+
+    print("Connected to the broker.")
+
+    while True:
+        for it in range(0, 10):
+            time.sleep(0.0015)
+            client.loop(0.0005)
+
+except Exception as ex:
+    print("ERROR: " + str(ex))
+    print("ERROR: " + traceback.format_exception(ex))
+finally:
+    GPIO.cleanup()
---
+++ client/playground/radar-client.py
@@ -0,0 +1,124 @@
+import pygame, sys, threading, random
+import paho.mqtt.client as mqtt
+import pyros.agent as agent
+
+pygame.init()
+bigFont = pygame.font.SysFont("arial", 32)
+frameclock = pygame.time.Clock()
+screen = pygame.display.set_mode((600,600))
+
+client = mqtt.Client("radar-client-#" + str(random.randint(1000, 9999)))
+
+roverAddress = ["172.24.1.184", "172.24.1.185", "172.24.1.186", "gcc-wifi-ap", "gcc-wifi-ap", "gcc-wifi-ap"]
+roverPort = [1883, 1883, 1883, 1884, 1885, 1886]
+selectedRover = 3
+
+connected = False
+stopped = False
+
+RADAR_AGENT = "radar-agent"
+
+
+def onConnect(client, data, rc):
+    global connected
+    if rc == 0:
+        print("DriveController: Connected to rover " + selectedRoverTxt + " @ " + roverAddress[selectedRover] + ".");
+        agent.init(client, RADAR_AGENT + ".py")
+        connected = True
+    else:
+        print("DriveController: Connection returned error result: " + str(rc))
+        sys.exit(rc)
+
+def onMessage(client, data, msg):
+    global exit
+
+    if agent.process(client, msg):
+        if agent.returncode(RADAR_AGENT) != None:
+            exit = True
+    else:
+        print("DriveController: Wrong topic '" + msg.topic + "'")
+
+
+def _reconnect():
+    client.reconnect()
+
+
+def connect():
+    global connected
+    connected = False
+    client.disconnect()
+    print("DriveController: Connecting to rover " + str(selectedRover + 2) + " @ " + roverAddress[selectedRover] + "...");
+
+    client.connect_async(roverAddress[selectedRover], roverPort[selectedRover], 60)
+    thread = threading.Thread(target=_reconnect)
+    thread.daemon = True
+    thread.start()
+
+
+def onDisconnect(client, data, rc):
+    connect()
+
+
+client.on_disconnect = onDisconnect
+client.on_connect = onConnect
+client.on_message = onMessage
+
+connect()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    keys = pygame.key.get_pressed()
+
+    client.loop(1/40)
+    screen.fill((0, 0, 0))
+
+    if keys[pygame.K_2]:
+        selectedRover = 0
+        connect()
+    elif keys[pygame.K_3]:
+        selectedRover = 1
+        connect()
+    elif keys[pygame.K_4]:
+        selectedRover = 2
+        connect()
+    elif keys[pygame.K_5]:
+        selectedRover = 3
+        connect()
+    elif keys[pygame.K_6]:
+        selectedRover = 4
+        connect()
+    elif keys[pygame.K_7]:
+        selectedRover = 5
+        connect()
+    elif keys[pygame.K_ESCAPE]:
+        client.publish("drive", "stop")
+        agent.stopAgent(client, RADAR_AGENT)
+        client.loop(0.7)
+        sys.exit(0)
+    elif keys[pygame.K_SPACE]:
+        if not stopped:
+            client.publish("drive", "stop")
+            agent.stopAgent(client, RADAR_AGENT)
+            stopped = True
+
+
+    selectedRoverTxt = str(selectedRover + 2)
+    if selectedRover > 2:
+        selectedRoverTxt = str(selectedRover - 1) + "-proxy"
+
+    if connected:
+        text = bigFont.render("Connected to rover: " + selectedRoverTxt + " @ " + roverAddress[selectedRover], 1, (128, 255, 128))
+    else:
+        text = bigFont.render("Connecting to rover: " + selectedRoverTxt + " @ " + roverAddress[selectedRover], 1, (255, 128, 128))
+    screen.blit(text, pygame.Rect(0, 0, 0, 0))
+
+    text = bigFont.render("Stopped: " + str(stopped), 1, (255, 255, 255))
+    screen.blit(text, pygame.Rect(0, 80, 0, 0))
+
+    pygame.display.flip()
+    frameclock.tick(30)
