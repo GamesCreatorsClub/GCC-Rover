43eadfd1ad98266a01cd58a65f7228f5782f959d
Daniel.Sendula <daniel.sendula@creative-sphere.com>
2017-03-14 09:14:58 +0000

    Added continuous read for camera

--- rover/camera_service.py
+++ rover/camera_service.py
@@ -14,13 +14,29 @@ from picamera import PiCamera
 # This service is fetching picture from camera...
 #
 
-DEBUG = False
+DEBUG = True
+CONTINUOUS_MODE_TIMEOUT = 5  # 5 seconds before giving up on sending accel data out
+MAX_TIMEOUT = 0.05  # 0.02 is 50 times a second so this is 50% longer
+
 
 whiteBalance = Image.new("L", (80, 64))
 
 camera = PiCamera()
 camera.resolution = (80, 64)
 stream = io.BytesIO()
+
+doReadSensor = False
+continuousMode = False
+lastTimeRead = 0
+lastTimeReceivedRequestForContMode = 0
+
+rawL = None
+rawRGB = None
+
+
+def log(msg):
+    if DEBUG:
+        print(msg)
 
 
 def capture():
@@ -48,29 +64,29 @@ def maxLevel(histogram, level):
     return len(histogram) - 1
 
 
-def limit(pixel, min, max):
-    if pixel > max:
-        pixel = max
-    if pixel < min:
-        pixel < min
+def limit(pixel, minPix, maxPix):
+    if pixel > maxPix:
+        pixel = maxPix
+    if pixel < minPix:
+        pixel < minPix
     return pixel
 
 
 def applyWhiteBalance(img, wb):
     histogram = img.histogram()
 
-    min = minLevel(histogram, 20)
-    max = maxLevel(histogram, 20)
+    minP = minLevel(histogram, 20)
+    maxP = maxLevel(histogram, 20)
 
     for y in range(0, 64):
         for x in range(0, 80):
             wbp = wb.getpixel((x, y))
-            wbp = limit(wbp, min, max)
+            wbp = limit(wbp, minP, maxP)
 
             p = img.getpixel((x, y))
-            offset = ((max - wbp) - min)
+            offset = ((maxP - wbp) - minP)
 
-            p = p + offset
+            p += offset
             if p > 255:
                 p = 255
             img.putpixel((x, y), p)
@@ -94,40 +110,43 @@ def blackAndWhite(img):
 def fetchRaw(topic, payload, groups):
     captured = capture()
 
-    print("  Asked to fetch raw image.")
+    log("  Asked to fetch raw image.")
     message = captured.tobytes("raw")
     pyroslib.publish("camera/raw", message)
-    print("  Sent raw image.")
+    log("  Sent raw image.")
 
 
-def fetchProcessed(topic, payload, groups):
-    print("  Asked to fetch processed image.")
+def handleFetchProcessed(topic, payload, groups):
+    log("  Asked to fetch processed image.")
+    fetchProcessed()
 
+
+def fetchProcessed():
     start = time.time()
-    print("    Capturing image...");
+    log("    Capturing image...")
     captured = capture()
-    print("    Captured image. Lasted " + str(time.time() - start) + "s")
+    log("    Captured image. Lasted " + str(time.time() - start) + "s")
 
     start2 = time.time()
     img = applyWhiteBalance(captured, whiteBalance)
-    print("    Processed white balance. Lasted " + str(time.time() - start2) + "s")
+    log("    Processed white balance. Lasted " + str(time.time() - start2) + "s")
 
     start2 = time.time()
     contrast = ImageEnhance.Contrast(img)
     finalImg = contrast.enhance(10)
-    print("    Processed contrast. Lasted " + str(time.time() - start2) + "s")
+    log("    Processed contrast. Lasted " + str(time.time() - start2) + "s")
 
     start2 = time.time()
     finalImg = blackAndWhite(finalImg)
-    print("    Set to black and white. Lasted " + str(time.time() - start2) + "s")
+    log("    Set to black and white. Lasted " + str(time.time() - start2) + "s")
 
     start2 = time.time()
     message = finalImg.tobytes("raw")
-    print("    Converted to bytes. Lasted " + str(time.time() - start2) + "s")
+    log("    Converted to bytes. Lasted " + str(time.time() - start2) + "s")
 
     start2 = time.time()
     pyroslib.publish("camera/processed", message)
-    print("    Published. Lasted " + str(time.time() - start2) + "s")
+    log("    Published. Lasted " + str(time.time() - start2) + "s")
 
     print("  Sent processed image. Total time " + str(time.time() - start) + "s")
 
@@ -146,6 +165,40 @@ def storeWhiteBalance(topic, payload, groups):
     captured.save("white-balance.png", "PNG")
     whiteBalance = captured
 
+
+def handleContinuousMode(topic, message, groups):
+    global doReadSensor, continuousMode, lastTimeReceivedRequestForContMode
+
+    if message.startswith("stop"):
+        continuousMode = False
+        doReadSensor = False
+        camera.stop_preview()
+        print("  Stopped continuous mode...")
+
+    else:
+        if not continuousMode:
+            continuousMode = True
+            doReadSensor = True
+            camera.start_preview()
+            print("  Started continuous mode...")
+
+        lastTimeReceivedRequestForContMode = time.time()
+
+
+def loop():
+    global doReadSensor, lastTimeRead, continuousMode
+
+    if doReadSensor:
+        fetchProcessed()
+        if continuousMode:
+            if time.time() - lastTimeReceivedRequestForContMode > CONTINUOUS_MODE_TIMEOUT:
+                continuousMode = False
+                camera.stop_preview()
+                print("  Stopped continuous mode.")
+        else:
+            doReadSensor = False
+
+
 if __name__ == "__main__":
     try:
         print("Starting camera service...")
@@ -156,14 +209,15 @@ if __name__ == "__main__":
             whiteBalance = whiteBalance.convert('L')
 
         pyroslib.subscribe("camera/raw/fetch", fetchRaw)
-        pyroslib.subscribe("camera/processed/fetch", fetchProcessed)
+        pyroslib.subscribe("camera/processed/fetch", handleFetchProcessed)
         pyroslib.subscribe("camera/whitebalance/fetch", fetchWhiteBalance)
         pyroslib.subscribe("camera/whitebalance/store", storeWhiteBalance)
+        pyroslib.subscribe("camera/continuous", handleContinuousMode)
         pyroslib.init("camera-service")
 
         print("Started camera service.")
 
-        pyroslib.forever(0.5)
+        pyroslib.forever(0.5, loop)
 
     except Exception as ex:
         print("ERROR: " + str(ex) + "\n" + ''.join(traceback.format_tb(ex.__traceback__)))
