c9b489732318cc38c0a0c0e0dd5735571fedabdd
Daniel.Sendula <daniel.sendula@creative-sphere.com>
2017-03-25 19:56:00 +0000

    Added more stub camera methods

--- RPi-stub/picamera.py
+++ RPi-stub/picamera.py
@@ -5,5 +5,11 @@ class PiCamera:
     def resolution(w, h):
         raise NotImplemented()
 
-    def capture(self, camera, type):
+    def capture(self, camera, type, use_video_port=True):
+        raise NotImplemented()
+
+    def start_preview(self):
+        raise NotImplemented()
+
+    def stop_preview(self):
         raise NotImplemented()
