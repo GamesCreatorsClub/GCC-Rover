03e3b33892769c2b9f04387119ae3b1cf0e46ece
Daniel.Sendula <daniel.sendula@creative-sphere.com>
2017-03-25 19:55:10 +0000

    Added follow the line client/agent pair

---
+++ client/linefollowing/follow-line-agent.py
@@ -0,0 +1,266 @@
+
+import sys
+import math
+import time
+import traceback
+import pygame
+import pyroslib
+from PIL import Image
+
+DEBUG = True
+
+TURNING_NONE = 0
+TURNING_WAIT = 1
+TURNING_DONE = 2
+
+MAX_TIMEOUT = 5
+
+run = False
+continuousMode = False
+
+lastPing = time.time()
+resubscribe = time.time()
+lastReceivedTime = time.time()
+
+receivedProcessedImage = Image.new("L", [80, 64])
+processedImage = receivedProcessedImage.copy()
+
+frameTime = ""
+turnDistance = 0
+feedback = ""
+
+minAngle = 0
+maxAngle = 0
+angle = 0
+forwardSpeed = 2
+
+turningState = TURNING_NONE
+
+
+def stop():
+    global run, continuousMode
+    print("Stopping...")
+    run = False
+    continuousMode = False
+    pyroslib.publish("move/stop", "stop")
+    pyroslib.publish("camera/continuous", "stop")
+    print("Stopped.")
+
+
+def start():
+    global run, continuousMode
+    print("Starting...")
+    run = True
+    continuousMode = True
+    pyroslib.publish("camera/continuous", "on")
+
+def connected():
+    stop()
+
+
+def handlePing(topic, message, groups):
+    global lastPing
+    lastPing = time.time()
+
+
+def handleSpeed(topic, message, group):
+    global forwardSpeed
+
+    forwardSpeed = int(message)
+
+
+def handleFeedbackMessage(topic, message, groups):
+    global turningState
+
+    turningState = TURNING_DONE
+    #
+    # if not continuousMode:
+    #     pyros.publish("camera/processed/fetch", "")
+
+
+def handleCommand(topic, message, groups):
+    if message == "start":
+        start()
+    elif message == "stop":
+        stop()
+    elif message == "oneStep":
+        goOneStep()
+
+
+def toPyImage(imageBytes):
+    return Image.frombytes("L", (80, 64), imageBytes)
+
+
+def processImage():
+    global processedImage, maxAngle, minAngle, angle, turnDistance
+
+    cameraImage = receivedProcessedImage.copy()
+
+    processedImage = Image.new("RGB", cameraImage.size)
+    processedImage.paste(cameraImage)
+
+    r = 28  # 28 pixels = 35mm
+
+    ha = math.atan2(0.5, r + 0.5) * 180 / math.pi
+    ha *= 2
+
+    # print("Angle is " + str(ha))
+    a = -90
+
+    p1 = 255
+    p2 = 0
+
+    minAngle = 90
+    maxAngle = -90
+    lastMinAngle = 90
+    lastMaxAngle = -90
+
+    pixelMap = processedImage.load()
+
+    c = 0
+    while a <= 90:
+        a += ha
+        ra = a * math.pi / 180 + math.pi
+
+        x = int(r * math.cos(ra) + 40)
+        y = int(r * math.sin(ra) + 32)
+
+        p = cameraImage.getpixel((x, y))
+        if p > 127:
+            pixelMap[x, y] = (0, 255, 0)
+        else:
+            pixelMap[x, y] = (255, 0, 0)
+            if a > maxAngle:
+                maxAngle = a
+            if a < minAngle:
+                minAngle = a
+
+        c += 1
+
+    midAngle = (maxAngle + minAngle) / 2
+
+    angle = round(midAngle, 1)
+
+    rDistance = 35  # 28 pixels = 35mm
+
+    if midAngle > 0:
+        a = 90 - midAngle
+
+        apr = a * math.pi / 180.0
+
+        divideWith = 2.0 * math.cos(apr)
+
+        if divideWith < 0.01:
+            turnDistance = 2000
+        else:
+            turnDistance = rDistance / divideWith
+            if turnDistance > 2000:
+                turnDistance = 2000
+            elif turnDistance < 60:
+                turnDistance = 60
+
+            turnDistance = - turnDistance
+    else:
+        a = 90 + midAngle
+
+        apr = a * math.pi / 180.0
+
+        divideWith = 2.0 * math.cos(apr)
+
+        if divideWith < 0.01:
+            turnDistance = 2000
+        else:
+            turnDistance = rDistance / divideWith
+            if turnDistance > 2000:
+                turnDistance = 2000
+            elif turnDistance < 60:
+                turnDistance = 60
+
+    turnDistance = int(turnDistance)
+
+    # print("Scanned " + str(c) + " points, midAngle=" + str(midAngle) + ", minAngle=" + str(minAngle) + ", maxAngle=" + str(maxAngle) + " turnDistance=" + str(turnDistance))
+
+
+def goOneStep():
+    global turningState
+
+    if abs(angle) < 20:
+        action = "drive 0 " + str(forwardSpeed)
+        pyroslib.publish("move/drive", "0 " + str(forwardSpeed))
+    elif abs(angle) < 60:
+        action = "steer " + str(turnDistance) + " " + str(forwardSpeed)
+        pyroslib.publish("move/steer", str(turnDistance) + " " + str(forwardSpeed))
+    else:
+        action = "turn " + str(-angle)
+        turningState = TURNING_WAIT
+        pyroslib.publish("move/turn", str(-angle))
+
+    message = processedImage.tobytes("raw")
+
+    pyroslib.publish("followLine/frameTime", frameTime)
+    pyroslib.publish("followLine/angle", str(round(angle, 1)))
+    pyroslib.publish("followLine/turnDistance", str(turnDistance))
+    pyroslib.publish("followLine/action", action)
+    pyroslib.publish("followLine/processed", message)
+
+    if DEBUG:
+        print("Angle " + str(angle) + ", turnDistance " + str(turnDistance) + ", action " + action + ", frame time " + frameTime)
+
+
+def handleCameraProcessed(topic, message, groups):
+    global receivedProcessedImage, processedImage
+    global frameTime, lastReceivedTime
+    global turningState
+
+    receivedProcessedImage = toPyImage(message)
+
+    processImage()
+
+    now = time.time()
+    if now - lastReceivedTime > 5:
+        frameTime = ">5s"
+    else:
+        frameTime = str(round(now - lastReceivedTime, 4))
+
+    lastReceivedTime = now
+
+    if turningState == TURNING_DONE:
+        turningState = TURNING_NONE
+    else:
+        if run and turningState == TURNING_NONE:
+            goOneStep()
+
+
+def loop():
+    global resubscribe
+
+    if continuousMode and time.time() - resubscribe > 2:
+        resubscribe = time.time()
+        if pyroslib.isConnected():
+            pyroslib.publish("camera/continuous", "on")
+
+    now = time.time()
+
+    if now - lastPing > MAX_TIMEOUT:
+        print("** Didn't receive ping for more than " + str(now - lastPing) + "s. Leaving...")
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    try:
+        print("Starting follow-line agent...")
+
+        pyroslib.subscribe("followLine/ping", handlePing)
+        pyroslib.subscribe("followLine/speed", handleSpeed)
+        pyroslib.subscribe("followLine/command", handleCommand)
+        pyroslib.subscribe("move/feedback", handleFeedbackMessage)
+        pyroslib.subscribeBinary("camera/processed", handleCameraProcessed)
+
+        pyroslib.init("follow-line-agent", unique=True)
+
+        print("Started follow-line agent.")
+
+        pyroslib.forever(0.02, loop)
+
+    except Exception as ex:
+        print("ERROR: " + str(ex) + "\n" + ''.join(traceback.format_tb(ex.__traceback__)))
---
+++ client/linefollowing/follow-line-client.py
@@ -0,0 +1,338 @@
+
+import sys
+import time
+import math
+import pygame
+import pyros
+import pyros.gcc
+import pyros.agent
+import pyros.pygamehelper
+from PIL import Image
+
+
+GREY = (160, 160, 160)
+
+MAX_PING_TIMEOUT = 1
+MAX_PICTURES = 400
+pingLastTime = 0
+
+pygame.init()
+bigFont = pygame.font.SysFont("arial", 32)
+font = pygame.font.SysFont("arial", 24)
+smallFont = pygame.font.SysFont("arial", 16)
+frameclock = pygame.time.Clock()
+screen = pygame.display.set_mode((1024, 800))
+
+cameraImage = Image.new("L", [80, 64])
+
+rawImage = pygame.Surface((80, 64), 24)
+receivedProcessedImage = pygame.Surface((80, 64), 24)
+whiteBalanceImage = pygame.Surface((80, 64), 24)
+
+
+rawImageBig = pygame.Surface((320, 256), 24)
+receivedProcessedImageBig = pygame.Surface((320, 256), 24)
+whiteBalanceImageBig = pygame.Surface((320, 256), 24)
+
+
+processedImages = []
+processedBigImages = []
+
+forwardSpeed = 2
+action = ""
+
+running = False
+
+lights = False
+resubscribe = time.time()
+lastReceivedTime = time.time()
+frameTime = ""
+
+receivedFrameTime = ""
+angle = 0
+turnDistance = 0
+
+imgNo = 0
+
+ptr = -1
+
+
+def connected():
+    pyros.publish("camera/processed/fetch", "")
+    pyros.agent.init(pyros.client, "follow-line-agent.py")
+    print("Sent agent")
+
+
+def toPyImage(imageBytes):
+    pilImage = Image.frombytes("L", (80, 64), imageBytes)
+
+    pilRGBImage = Image.new("RGB", pilImage.size)
+    pilRGBImage.paste(pilImage)
+    return toPyImage3(pilRGBImage)
+
+
+def toPyImage3(pilRGBImage):
+    pyImageSmall = pygame.image.fromstring(pilRGBImage.tobytes("raw"), (80, 64), 'RGB')
+    pyImageBig = pygame.transform.scale(pyImageSmall, (320, 256))
+    return pyImageSmall, pyImageBig
+
+
+def handleWhiteBalance(topic, message, groups):
+    global whiteBalanceImage, whiteBalanceImageBig
+
+    images = toPyImage(message)
+    whiteBalanceImage = images[0]
+    whiteBalanceImageBig = images[1]
+    print("  Converted images for white balance.")
+
+
+def handleCameraRaw(topic, message, groups):
+    global rawImage, rawImageBig
+
+    images = toPyImage(message)
+    rawImage = images[0]
+    rawImageBig = images[1]
+    print("  Converted images for raw.")
+
+
+def handleCameraProcessed(topic, message, groups):
+    global receivedProcessedImage, receivedProcessedImageBig
+
+    images = toPyImage(message)
+    receivedProcessedImage = images[0]
+    receivedProcessedImageBig = images[1]
+
+
+def handleAgentProcessed(topic, message, groups):
+    global processedImages, processedBigImages
+    global frameTime, lastReceivedTime
+    global imgNo
+
+    pilRGBImage = Image.frombytes("RGB", (80, 64), message)
+    images = toPyImage3(pilRGBImage)
+
+    processedImage = images[0]
+    processedImageBig = images[1]
+
+    t = smallFont.render("A: " + str(angle), 1, GREY)
+    processedImageBig.blit(t, (0, 0))
+
+    t = smallFont.render("T: " + str(turnDistance), 1, GREY)
+    processedImageBig.blit(t, (220, 0))
+
+    t = smallFont.render("#: " + str(imgNo), 1, GREY)
+    processedImageBig.blit(t, (0, 230))
+
+    t = smallFont.render(action, 1, GREY)
+    processedImageBig.blit(t, (180, 230))
+
+    t = smallFont.render("T: " + receivedFrameTime, 1, GREY)
+    processedImageBig.blit(t, (180, 210))
+
+    imgNo += 1
+
+    pa = (angle + 180) * math.pi / 180
+
+    lx = 100 * math.cos(pa) + 160
+    ly = 100 * math.sin(pa) + 128
+
+    pygame.draw.line(processedImageBig, (127, 127, 127), (160, 128), (lx, ly))
+
+    processedImages.append(processedImage)
+    processedBigImages.append(processedImageBig)
+
+    if len(processedImages) > MAX_PICTURES:
+        del processedImages[0]
+        del processedBigImages[0]
+
+    now = time.time()
+    if now - lastReceivedTime > 5:
+        frameTime = ">5s"
+    else:
+        frameTime = str(round(now - lastReceivedTime, 2))
+
+    lastReceivedTime = now
+
+
+def handleAngle(topic, message, groups):
+    global angle
+
+    angle = float(message)
+
+
+def handleAction(topic, message, groups):
+    global action
+
+    action = message
+
+
+def handleTurnDistance(topic, message, groups):
+    global turnDistance
+
+    turnDistance = float(message)
+
+
+def handleFrameTime(topic, message, groups):
+    global receivedFrameTime
+
+    receivedFrameTime = message
+
+
+def goOneStep():
+    pyros.publish("followLine/command", "oneStep")
+
+
+def goForward():
+    print("** Going forward " + str(forwardSpeed))
+    pyros.publish("move/drive", "0 " + str(forwardSpeed))
+    pyros.loop(0.5)
+    pyros.publish("move/stop", "")
+    pyros.loop(1)
+    pyros.publish("camera/processed/fetch", "")
+
+
+def onKeyDown(key):
+    global lights, forwardSpeed, running, ptr, imgNo
+    global processedImages, processedBigImages
+
+    if key == pygame.K_ESCAPE:
+        sys.exit()
+    elif key == pygame.K_w:
+        print("  fetching white balance picture...")
+        pyros.publish("camera/whitebalance/fetch", "")
+    elif key == pygame.K_r:
+        print("  fetching white balance picture...")
+        pyros.publish("camera/raw/fetch", "")
+    elif key == pygame.K_p:
+        print("  fetching processed picture...")
+        pyros.publish("camera/processed/fetch", "")
+    elif key == pygame.K_s:
+        print("  storing whitebalance image...")
+        pyros.publish("camera/whitebalance/store", "")
+        pyros.publish("camera/whitebalance/fetch", "")
+    elif key == pygame.K_l:
+        if lights:
+            print("  switching off lights")
+            pyros.publish("lights/camera", "off")
+            lights = False
+        else:
+            print("  switching on lights")
+            pyros.publish("lights/camera", "on")
+            lights = True
+    elif key == pygame.K_g:
+        goOneStep()
+    elif key == pygame.K_LEFT:
+        if ptr == -1:
+            ptr = len(processedImages) - 2
+        else:
+            ptr -= 1
+    elif key == pygame.K_RIGHT:
+        ptr += 1
+        if ptr >= len(processedImages) - 1:
+            ptr -= 1
+    elif key == pygame.K_RETURN:
+        running = not running
+        if running:
+            pyros.publish("followLine/command", "start")
+            imgNo = 0
+            del processedImages[:]
+            del processedBigImages[:]
+        else:
+            pyros.publish("move/stop", "")
+            pyros.publish("followLine/command", "stop")
+    elif key == pygame.K_LEFTBRACKET:
+        forwardSpeed -= 1
+        if forwardSpeed < 0:
+            forwardSpeed = 0
+        pyros.publish("followLine/speed", str(forwardSpeed))
+    elif key == pygame.K_RIGHTBRACKET:
+        forwardSpeed += 1
+        pyros.publish("followLine/speed", str(forwardSpeed))
+    elif key == pygame.K_SPACE:
+        pyros.publish("move/stop", "")
+        pyros.publish("followLine/command", "stop")
+        running = False
+    else:
+        pyros.gcc.handleConnectKeys(key)
+
+
+def onKeyUp(key):
+    return
+
+
+pyros.subscribeBinary("camera/whitebalance", handleWhiteBalance)
+pyros.subscribeBinary("camera/raw", handleCameraRaw)
+pyros.subscribeBinary("camera/processed", handleCameraProcessed)
+pyros.subscribeBinary("followLine/processed", handleAgentProcessed)
+pyros.subscribe("followLine/angle", handleAngle)
+pyros.subscribe("followLine/turnDistance", handleTurnDistance)
+pyros.subscribe("followLine/action", handleAction)
+pyros.subscribe("followLine/frameTime", handleFrameTime)
+pyros.init("camera-display-#", unique=True, onConnected=connected, host=pyros.gcc.getHost(), port=pyros.gcc.getPort(), waitToConnect=False)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pyros.pygamehelper.processKeys(onKeyDown, onKeyUp)
+
+    pyros.loop(0.03)
+
+    screen.fill((0, 0, 0))
+
+    if pyros.isConnected():
+        text = bigFont.render("Connected to rover: " + pyros.gcc.getSelectedRoverDetailsText(), 1, (128, 255, 128))
+    else:
+        text = bigFont.render("Connecting to rover: " + pyros.gcc.getSelectedRoverDetailsText(), 1, (255, 128, 128))
+
+    screen.blit(text, (0, 0))
+
+    text = font.render("Selected: " + str(ptr), 1, (255, 255, 255))
+    screen.blit(text, (400, 50))
+
+    text = font.render("Frame time: " + str(frameTime), 1, (255, 255, 255))
+    screen.blit(text, (400, 80))
+
+    text = font.render("Angle: " + str(round(angle, 2)), 1, (255, 255, 255))
+    screen.blit(text, (400, 110))
+
+    text = font.render("Speed: " + str(forwardSpeed), 1, (255, 255, 255))
+    screen.blit(text, (750, 50))
+
+    text = font.render("Running: " + str(running), 1, (255, 255, 255))
+    screen.blit(text, (750, 80))
+
+    text = font.render("Turn dist: " + str(round(turnDistance, 2)), 1, (255, 255, 255))
+    screen.blit(text, (750, 110))
+
+    screen.blit(rawImage, (10, 50))
+    screen.blit(whiteBalanceImage, (110, 50))
+    screen.blit(receivedProcessedImage, (210, 50))
+
+    screen.blit(rawImageBig, (10, 150))
+    screen.blit(whiteBalanceImageBig, (362, 150))
+    screen.blit(receivedProcessedImageBig, (724, 150))
+
+    if ptr >= 0:
+        if ptr > len(processedImages) - 1:
+            ptr = len(processedImages) - 1
+        i = ptr
+    else:
+        i = len(processedImages) - 1
+    x = 724
+    while i >= 0 and x >= 0:
+        screen.blit(processedBigImages[i], (x, 420))
+        x -= 362
+        i -= 1
+
+    pygame.display.flip()
+    frameclock.tick(30)
+
+    now = time.time()
+
+    if now - pingLastTime > MAX_PING_TIMEOUT:
+        pyros.publish("followLine/ping", "")
+        pingLastTime = now
