13261d559d516d561bbbdccea73f115c56868dec
natdan <natdan@github.com>
2018-02-19 08:44:56 +0000

    Fixed upload all

--- rover/upload-all.sh
+++ rover/upload-all.sh
@@ -82,7 +82,7 @@ pyros $1 restart   9dofsensor
 
 echo ""
 echo Uploading     vl53l0x
-pyros $1 upload -s vl53l0x vl53l0x/vl53l0x_service.py -e vl53l0x/vl53l0xapi.py vl53l0x/vl53l0x_python.so
+pyros $1 upload -s vl53l0x vl53l0x/vl53l0x_service.py -e vl53l0x/vl53l0xWrapper.py vl53l0x/vl53l0x_python.so vl53l0x/vl53l0xPython.py
 echo Restarting    vl53l0x
 pyros $1 restart   vl53l0x
 
