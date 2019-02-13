4bebcfcdb55347b114e9b0fd1cc2c69ce062d4bc
Daniel Sendula <natdan@users.noreply.github.com>
2019-02-13 17:00:13 +0000

    Promoted canyons of mars out of playground

---
+++ client/canyons-of-mars/canyons-of-mars-agent.py
@@ -0,0 +1,175 @@
+
+#
+# Copyright 2016-2019 Games Creators Club
+#
+# MIT License
+#
+
+import time
+import traceback
+
+import pyroslib
+
+DEBUG_LEVEL_OFF = 0
+DEBUG_LEVEL_INFO = 1
+DEBUG_LEVEL_DEBUG = 2
+DEBUG_LEVEL_ALL = 3
+DEBUG_LEVEL = DEBUG_LEVEL_ALL
+
+
+lastOdo = [5, 6, 7, 8]
+odo = [0, 0, 0, 0]
+requiredOdo = [5, 6, 7, 8]
+
+timeToSendData = 0
+running = False
+
+
+def doNothing():
+    pass
+
+
+lastProcessed = time.time()
+
+
+def deltaDeg(old, new):
+    d = new - old
+    if d > 32768:
+        d -= 32768
+    elif d < -32768:
+        d += 32768
+
+    # 100, 102 -> 2
+    # 102, 100 -> -2
+
+    # 359, 1 -> -358 -> 2
+    # 1, 359 -> 358 -> -2
+
+    return d
+
+
+def formatArgL(label, value, fieldSize):
+    if len(label) > 0:
+        return label + ":" + str(value).ljust(fieldSize)
+    else:
+        return str(value).ljust(fieldSize)
+
+
+def formatArgR(label, value, fieldSize):
+    if len(label) > 0:
+        return label + ":" + str(value).rjust(fieldSize)
+    else:
+        return str(value).rjust(fieldSize)
+
+
+def log(level, what):
+    if level <= DEBUG_LEVEL:
+        print(what)
+
+
+def logArgs(*msg):
+    tnow = time.time()
+
+    logMsg = formatArgR("", int(tnow * 1000) % 100000, 7) + " " + " ".join(msg)
+    log(DEBUG_LEVEL_DEBUG, logMsg)
+
+
+def connected():
+    # pyroslib.publish("sensor/gyro/continuous", "calibrate,50")
+    pass
+
+
+def handleOdo(topic, message, groups):
+    data = message.split(",")
+
+    deltas = [0, 0, 0, 0]
+    for i in range(4):
+        d = i * 2
+        if data[d + 2] == "0":
+            newOdo = int(data[d + 1])
+            d = deltaDeg(lastOdo[i], newOdo)
+            if i == 2:
+                d = -d
+
+            deltas[i] = d
+            odo[i] += d
+            lastOdo[i] = newOdo
+    print(str(odo) + ", " + str(deltas) + ", " + str(lastOdo))
+
+
+def handleAgentCommands(topic, message, groups):
+    data = message.split(" ")
+
+    print("Command " + message)
+
+    cmd = data[0]
+
+    if cmd == "stop":
+        stop()
+    elif cmd == "start":
+        start(data[1])
+
+
+def stop():
+    global running
+
+    pyroslib.publish("move/stop", "")
+
+    running = False
+
+    log(DEBUG_LEVEL_ALL, "Stopped driving...")
+
+
+def start(distanceStr):
+    global running
+
+    distance = int((float(distanceStr) / 360) * 4096)
+    for i in range(4):
+        odo[i] = 0
+        requiredOdo[i] = distance
+
+    running = True
+
+    # pyroslib.publish("move/drive", "0 200")
+    pyroslib.publish("move/steer", "300 120")
+
+    log(DEBUG_LEVEL_ALL, "Started driving... for  " + distanceStr + " (" + str(requiredOdo) + ")")
+
+
+def mainLoop():
+    global timeToSendData
+
+    def sendData():
+        pyroslib.publish("canyons/odo", ",".join([str(x) for x in odo]))
+
+    now = time.time()
+
+    if timeToSendData <= now:
+        sendData()
+        timeToSendData = now + 0.2
+
+    if running:
+        stopped = False
+        for i in range(4):
+            if odo[i] >= requiredOdo[i]:
+                stop()
+                stopped = True
+        if not stopped:
+            print(str(odo) + " <-> " + str(requiredOdo))
+
+
+if __name__ == "__main__":
+    try:
+        print("Starting canyons-of-mars agent...")
+
+        pyroslib.subscribe("canyons/command", handleAgentCommands)
+        pyroslib.subscribe("wheel/speed/status", handleOdo)
+
+        pyroslib.init("canyons-of-mars-agent", unique=True, onConnected=connected)
+
+        print("Started canyons-of-mars agent.")
+
+        pyroslib.forever(0.02, mainLoop)
+
+    except Exception as ex:
+        print("ERROR: " + str(ex) + "\n" + ''.join(traceback.format_tb(ex.__traceback__)))
---
+++ client/canyons-of-mars/canyons-of-mars-client.py
@@ -0,0 +1,132 @@
+
+#
+# Copyright 2016-2019 Games Creators Club
+#
+# MIT License
+#
+
+import sys
+import time
+import pygame
+import pyros
+import pyros.gcc
+import pyros.gccui
+import pyros.agent
+import pyros.pygamehelper
+
+
+MAX_PING_TIMEOUT = 1
+
+pingLastTime = 0
+
+
+screen_size = (800, 640)
+
+screen = pyros.gccui.initAll(screen_size, True)
+
+arrow_image = pygame.image.load("arrow.png")
+arrow_image = pygame.transform.scale(arrow_image, (50, 50))
+
+
+gyroAngle = 0
+
+running = False
+
+odos = [1, 2, 3, 4]
+
+
+def handleData(topic, message, groups):
+    global odos
+    odos = [int(x) for x in message.split(",")]
+
+
+def connected():
+    print("Starting agent... ", end="")
+    pyros.agent.init(pyros.client, "canyons-of-mars-agent.py")
+    pyros.subscribe("canyons/odo", handleData)
+    print("Done.")
+
+
+def toggleStart():
+    global running
+
+    pass
+
+
+def stop():
+    global running
+    pyros.publish("canyons/command", "stop")
+    running = False
+
+
+def start():
+    global running
+    pyros.publish("canyons/command", "start " + str(360 * 2.2))
+    running = True
+
+
+def onKeyDown(key):
+    global running
+
+    if pyros.gcc.handleConnectKeyDown(key):
+        pass
+    elif key == pygame.K_SPACE:
+        print("Sending stop...")
+        stop()
+    elif key == pygame.K_RETURN:
+        print("Sending start...")
+        start()
+
+
+def onKeyUp(key):
+    pyros.gcc.handleConnectKeyUp(key)
+    return
+
+
+# pyros.subscribe("overtherainbow/gyro", handleGyro)
+
+pyros.init("canyons-of-mars-#", unique=True, onConnected=connected, host=pyros.gcc.getHost(), port=pyros.gcc.getPort(), waitToConnect=False)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.VIDEORESIZE:
+            pyros.gccui.screenResized(event.size)
+
+    pyros.pygamehelper.processKeys(onKeyDown, onKeyUp)
+
+    pyros.loop(0.03)
+    pyros.agent.keepAgents()
+    pyros.gccui.background(True)
+
+    # noinspection PyRedeclaration
+    hpos = 40
+    hpos = pyros.gccui.drawKeyValue("Running", str(running), 8, hpos)
+
+    hpos = pyros.gccui.drawKeyValue("FL", str(odos[0]), 20, hpos)
+    hpos = pyros.gccui.drawKeyValue("FR", str(odos[1]), 20, hpos)
+    hpos = pyros.gccui.drawKeyValue("BL", str(odos[2]), 20, hpos)
+    hpos = pyros.gccui.drawKeyValue("BR", str(odos[3]), 20, hpos)
+
+    loc = arrow_image.get_rect().center
+    rot_arrow_image = pygame.transform.rotate(arrow_image, -gyroAngle)
+    rot_arrow_image.get_rect().center = loc
+    screen.blit(rot_arrow_image, (530, 200))
+
+    # if len(rotSpeeds) > 0:
+    #     gyroDegPersSecText = str(round(sum(rotSpeeds) / len(rotSpeeds), 2))
+    #     pyros.gccui.drawBigText(gyroDegPersSecText, (440, 10))
+    #
+    #     pyros.gccui.drawText("º/s", (445 + pyros.gccui.bigFont.size(gyroDegPersSecText)[0], 15))
+    #
+    #     pyros.gccui.drawBigText(str(int(thisGyroAngle)), (440, 40))
+
+    pyros.gccui.drawSmallText("Put help here", (8, screen.get_height() - pyros.gccui.smallFont.get_height()))
+
+    pyros.gcc.drawConnection()
+    pyros.gccui.frameEnd()
+
+    now = time.time()
