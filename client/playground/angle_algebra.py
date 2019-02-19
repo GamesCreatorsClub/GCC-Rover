fbd86738106c1efeaaa680931d7fef5cd8e613b0
Daniel Sendula <natdan@users.noreply.github.com>
2019-02-19 18:42:32 +0000

    Tests for angular algebra

--- client/playground/angle_algebra.py
+++ client/playground/angle_algebra.py
@@ -1,64 +1,45 @@
-def normaiseAngle(a):
-    if a < 0:
-        a += 360
-    if a >= 360:
-        a -= 360
-    return a
+
+from  wheels_service import smallestAngleChange
 
 
-def angleDiference(a1, a2):
-    diff = a1 - a2
-    if diff > 180:
-        return diff - 360
-    elif diff < -180:
-        return diff + 360
-    else:
-        return diff
+def test(old_angle_mod, new_angle, expected_angle_mod):
+    res = smallestAngleChange(old_angle_mod[0], old_angle_mod[1], new_angle)
+
+    if res != expected_angle_mod:
+        print("Expected (" + str(old_angle_mod[0]) + ", " + str(old_angle_mod[1]) + ") • " + str(new_angle) + " -> (" + str(expected_angle_mod[0]) + ", " + str(expected_angle_mod[1]) + ")")
+        print("But got  (" + str(old_angle_mod[0]) + ", " + str(old_angle_mod[1]) + ") • " + str(new_angle) + " -> (" + str(res[0]) + ", " + str(res[1]) + ")")
+        raise AssertionError
 
 
-def addAngles(a1, a2):
-    return normaiseAngle(a1 + a2)
+test((0, 1), 0, (0, 1))
+test((0, 1), 45, (45, 1))
+test((45, 1), 0, (0, 1))
 
+test((45, 1), 270, (90, -1))
+test((45, -1), 270, (90, -1))
+test((270, 1), 45, (225, -1))
+test((270, -1), 45, (225, -1))
 
-def subAngles(a1, a2):
-    return normaiseAngle(a1 - a2)
+test((315, -1), 130, (310, -1))
+test((315, -1), 135, (315, -1))
+test((315, -1), 140, (320, -1))
 
+test((315, 1), 130, (310, -1))
+test((315, 1), 135, (315, -1))
+test((315, 1), 140, (320, -1))
 
-def oppositeAngle(a, mod):
-    if mod >= 0:
-        return a
-    return normaiseAngle(a + 180)
+test((130, -1), 315, (135, -1))
+test((135, -1), 315, (135, -1))
+test((140, -1), 315, (135, -1))
 
+test((130, 1), 315, (135, -1))
+test((135, 1), 315, (135, -1))
+test((140, 1), 315, (135, -1))
 
-def smallestAngleChange(old_angle, mod, new_angle):
-    real_old_angle = oppositeAngle(old_angle, mod)
-    angle_diff = angleDiference(real_old_angle, new_angle)
-    if angle_diff > 90:
-        new_diff = angle_diff - 180
-        return normaiseAngle(old_angle + new_diff), -mod
-    elif angle_diff < -90:
-        new_diff = 180 - angle_diff
-        return normaiseAngle(old_angle + new_diff), -mod
-    elif mod < 0:
-        return normaiseAngle(old_angle + angle_diff), mod
-    else:
-        return new_angle, mod
+test((315, 1), 270, (270, 1))
+test((315, 1), 90, (270, -1))
+test((315, -1), 270, (270, 1))
+test((315, -1), 90, (270, -1))
 
-
-new_angle, mod = smallestAngleChange(0, 1, 135)
-print(new_angle, mod)
-
-new_angle, mod = smallestAngleChange(315, -1, 135)
-print(new_angle, mod)
-
-new_angle, mod = smallestAngleChange(315, -1, 130)
-print(new_angle, mod)
-
-new_angle, mod = smallestAngleChange(315, -1, 140)
-print(new_angle, mod)
-
-new_angle, mod = smallestAngleChange(315, 1, 130)
-print(new_angle, mod)
-
-new_angle, mod = smallestAngleChange(315, 1, 270)
-print(new_angle, mod)
+test((315, 1), 45, (45, 1))
+test((315, -1), 45, (225, -1))
