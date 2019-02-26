64fa958bb0da1c6e1d2dd5d8b32d8602711d454f
Daniel Sendula <natdan@users.noreply.github.com>
2019-02-26 14:16:21 +0000

    Some work on canyons of mars

--- client/canyons-of-mars/canyons-of-mars-agent.py
+++ client/canyons-of-mars/canyons-of-mars-agent.py
@@ -6,16 +6,151 @@
 #
 
 import math
+import struct
 import time
 import traceback
 
 import pyroslib
 import pyroslib.logging
-from pyroslib.logging import log, DEBUG_LEVEL_ALWAYS, DEBUG_LEVEL_INFO, DEBUG_LEVEL_DEBUG
+from pyroslib.logging import log, LOG_LEVEL_ALWAYS, LOG_LEVEL_INFO, LOG_LEVEL_DEBUG
 
-pyroslib.logging.DEBUG_LEVEL = DEBUG_LEVEL_INFO
+pyroslib.logging.LOG_LEVEL = LOG_LEVEL_INFO
 
 sqrt2 = math.sqrt(2)
+
+wheel_names = ['fl', 'fr', 'bl', 'bl']
+
+
+class Heading:
+    def __init__(self):
+        self.current_heading = 0
+        self.start_heading = 0
+
+    def heading(self):
+        return self.heading
+
+    def reset(self):
+        self.start_heading = -1
+
+    def process(self, heading):
+        if heading < 0:
+            heading += 360
+        if self.start_heading == -1:
+            self.start_heading = heading
+        else:
+            heading -= self.start_heading
+        if heading < 0:
+            heading += 360
+
+        self.current_heading = heading
+
+
+class Odo:
+    def __init__(self):
+        self.odo = {'fl':0, 'fr':0, 'bl':0, 'br':0}
+        self.last_odo = {'fl':0, 'fr':0, 'bl':0, 'br':0}
+        self.wheel_speeds = {'fl':0, 'fr':0, 'bl':0, 'br':0}
+        self.last_odo_data = 0
+        self.odo_delta_time = 0
+
+        self.wheel_orientation = {'fl':0, 'fr':0, 'bl':0, 'br':0}
+        self.last_wheel_orientation = {'fl':0, 'fr':0, 'bl':0, 'br':0}
+        self.wheel_rot_speeds = {'fl':0, 'fr':0, 'bl':0, 'br':0}
+        self.last_speed_data = 0
+        self.speed_delta_time = 0
+
+    def wheelOdos(self):
+        return self.odo
+
+    def wheelSpeeds(self):
+        return self.wheel_speeds
+
+    def wheelSpeedAndDirection(self):
+        def addAngles(v1, v2):
+            t = (v1[1] + 180 - v2[1]) * math.pi / 180
+            m = math.sqrt(v1[0] * v1[0] + v2[0] * v2[0] - 2 * v1[0] * v1[0] * math.cos(t))
+            if m < 0.00001:
+                return 0, 0
+
+            a = math.asin(v2[0] * math.sin(t) / m)
+            return m, a * 180 / math.pi
+
+        if self.odo_delta_time == 0:
+            return 0
+        total = (0, 0)
+        for wheel in wheel_names:
+            distance = self.odo[wheel] - self.last_odo[wheel]
+            total = addAngles(total, (distance, self.wheel_orientation[wheel]))
+
+        return total[0] / (4 * self.odo_delta_time) , total[1]
+
+    def wheelOrietations(self):
+        return self.wheel_orientation
+
+    def wheelOrientationalSpeeda(self):
+        return self.wheel_rot_speeds
+
+    def processSpeed(self, data):
+        def deltaOdo(old, new):
+            d = new - old
+            if d > 32768:
+                d -= 32768
+            elif d < -32768:
+                d += 32768
+
+            return d
+
+        t = float(data[0])
+
+        if t - self.last_odo_data < 1:
+            self.odo_delta_time = t - self.last_odo_data
+        else:
+            self.odo_delta_time = 100000000
+
+        self.last_odo_data = t
+
+        for i in range(4):
+            data_index = i * 2
+            if data[data_index + 2] == "0":
+                new_odo = int(data[data_index + 1])
+                delta_odo = deltaOdo(self.last_odo[wheel_names[i]], new_odo)
+                self.last_odo[wheel_names[i]] = new_odo
+                self.odo[wheel_names[i]] += delta_odo
+
+
+    def processOrientation(self, data):
+        def deltaDeg(old, new):
+            d = (int(new) - int(old)) % 360
+            if d < 0:
+                d += 360
+            return d
+
+        for i in range(4):
+            data_index = i * 2
+            if data[data_index + 2] == "32":
+                new_deg = int(data[data_index + 1])
+                delta_deg = deltaDeg(self.last_wheel_orientation[wheel_names[i]], new_deg)
+                self.last_wheel_orientation[wheel_names[i]] = new_deg
+                self.wheel_orientation[wheel_names[i]] += delta_deg
+
+
+class Radar:
+    def __init__(self):
+        self.radar = {0: 0, 45: 0, 90: 0, 135: 0, 180: 0, 225: 0, 270: 0, 315: 0}
+        self.last_radar = {0: 0, 45: 0, 90: 0, 135: 0, 180: 0, 225: 0, 270: 0, 315: 0}
+
+    def process(self, values):
+        for d in self.radar:
+            self.last_radar[d] = self.radar[d]
+
+        for (k,v) in values:
+            if k == 'timestamp':
+                timestamp = float(v)
+            else:
+                self.radar[int(k)] = int(v)
+
+    def __getitem__(self, item):
+        return self.radar[item]
 
 
 class Action:
@@ -53,7 +188,7 @@ class StopAction(Action):
         self.parent.running = False
         pyroslib.publish("move/stop", "")
         pyroslib.publish("canyons/feedback/running", "False")
-        log(DEBUG_LEVEL_ALWAYS, "Stopped driving...")
+        log(LOG_LEVEL_ALWAYS, "Stopped driving...")
 
     def execute(self):
         return self.parent.do_nothing
@@ -68,7 +203,7 @@ class MoveForwardOnOdo(Action):
         self.odo = odo
         self.radar = radar
         self.stop_action = stop_action
-        self.required_odo = [0, 0, 0, 0]
+        self.required_odo = {'fl':0, 'fr':0, 'bl':0, 'br':0}
 
     def setRequiredOdo(self, distance):
         for i in range(len(self.odo)):
@@ -76,9 +211,9 @@ class MoveForwardOnOdo(Action):
 
     def start(self):
         super(MoveForwardOnOdo, self).start()
-        for i in range(len(self.odo)):
-            self.odo[i] = 0
-        log(DEBUG_LEVEL_DEBUG, "Reset odo to " + str(self.odo) + ", required odo  " + str(self.required_odo) + "; starting...")
+        for wheel in self.odo:
+            self.odo.wheelOdos()[wheel] = 0
+        log(LOG_LEVEL_DEBUG, "Reset odo to " + str(self.odo) + ", required odo  " + str(self.required_odo) + "; starting...")
 
         pyroslib.publish("move/steer", "300 120")
 
@@ -87,7 +222,7 @@ class MoveForwardOnOdo(Action):
 
     def execute(self):
         do_stop = False
-        log(DEBUG_LEVEL_DEBUG, "Driving " + str(self.odo) + " to  " + str(self.required_odo))
+        log(LOG_LEVEL_DEBUG, "Driving " + str(self.odo) + " to  " + str(self.required_odo))
         for i in range(4):
             if self.odo[i] >= self.required_odo[i]:
                 do_stop = True
@@ -105,9 +240,11 @@ class MoveForwardOnOdo(Action):
 
 
 class MazeAction(Action):
-    def __init__(self, radar):
+    def __init__(self, odo, radar, heading):
         super(MazeAction, self).__init__()
+        self.odo = odo
         self.radar = radar
+        self.heading = heading
 
     def check_next_action_conditions(self):
         return self
@@ -117,8 +254,8 @@ class MazeCorridorAction(MazeAction):
     LEFT = -1
     RIGHT = 1
 
-    def __init__(self, radar, left_or_right, distance, speed):
-        super(MazeCorridorAction, self).__init__(radar)
+    def __init__(self, odo, radar, heading, left_or_right, distance, speed):
+        super(MazeCorridorAction, self).__init__(odo, radar, heading)
         self.left_or_right = left_or_right
         self.distance = distance
         self.speed = speed
@@ -178,7 +315,90 @@ class MazeCorridorAction(MazeAction):
 
         distance = int(distance)
 
-        log(DEBUG_LEVEL_INFO, "Distance is " + str(distance))
+        log(LOG_LEVEL_INFO, "Distance is " + str(distance))
+
+        pyroslib.publish("move/steer", str(distance) + " " + str(self.speed))
+
+        pyroslib.publish("canyons/feedback/corridor",
+                         str(int(self.radar[0])) +
+                         " " + str(int(self.radar[180])) +
+                         " " + str(int(left_angle)) +
+                         " " + str(int(right_angle)) +
+                         " " + str(int(left_front_distance)) +
+                         " " + str(int(right_front_distance))
+                         )
+        return self
+
+    def getActionName(self):
+        return "Corridor"
+
+
+class MazeCornerAction(MazeAction):
+    LEFT = -1
+    RIGHT = 1
+
+    def __init__(self, odo, radar, heading, left_or_right, distance, speed):
+        super(MazeCornerAction, self).__init__(odo, radar, heading)
+        self.left_or_right = left_or_right
+        self.distance = distance
+        self.speed = speed
+        if self.left_or_right == MazeCorridorAction.RIGHT:
+            self.a1 = 45
+            self.a2 = 90
+            self.a3 = 135
+        else:
+            self.a1 = 315
+            self.a2 = 270
+            self.a3 = 225
+
+    def start(self):
+        super(MazeCornerAction, self).start()
+
+    def end(self):
+        super(MazeCornerAction, self).end()
+
+    def execute(self):
+
+        def calculateAngleAndFrontDistance(df, dm, db):
+            dfsqrt2 = df / sqrt2
+            dbsqrt2 = db / sqrt2
+
+            if df < db:
+                angle = math.atan2(dfsqrt2, dfsqrt2 - dm) * 180 / math.pi - 90
+            else:
+                angle = 90 - math.atan2(dbsqrt2, dbsqrt2 - dm) * 180 / math.pi
+
+            xf, yf = dfsqrt2, dfsqrt2
+            xm, ym = dm, 0
+            xb, yb = dbsqrt2, -dbsqrt2
+
+            d = ((ym - yb) * xf + (xb - xm) * yf + (xm * yb - xb * ym)) / math.sqrt((xb - xm) * (xb - xm) + (yb - ym) * (yb - ym))
+
+            return angle, d
+
+        left_angle, left_front_distance = calculateAngleAndFrontDistance(self.radar[315], self.radar[270], self.radar[225])
+        right_angle, right_front_distance = calculateAngleAndFrontDistance(self.radar[45], self.radar[90], self.radar[135])
+
+        gain = 60
+        offset = 150
+
+        if right_angle > 40:
+            distance = -offset
+        elif right_angle < -40:
+            distance = offset
+        elif right_angle > 0:
+            distance = 40 - right_angle
+            distance *= -gain
+
+            distance -= offset
+        else:
+            distance = 40 + right_angle
+            distance *= gain
+            distance += offset
+
+        distance = int(distance)
+
+        log(LOG_LEVEL_INFO, "Distance is " + str(distance))
 
         pyroslib.publish("move/steer", str(distance) + " " + str(self.speed))
 
@@ -199,10 +419,9 @@ class MazeCorridorAction(MazeAction):
 class CanyonsOfMarsAgent:
     def __init__(self):
         self.running = False
-        self.odo = [0, 0, 0, 0]
-        self.last_odo = [0, 0, 0, 0]
-        self.radar = {0: 0, 45: 0, 90: 0, 135: 0, 180: 0, 225: 0, 270: 0, 315: 0}
-        self.last_radar = {0: 0, 45: 0, 90: 0, 135: 0, 180: 0, 225: 0, 270: 0, 315: 0}
+        self.odo = Odo()
+        self.radar = Radar()
+        self.heading = Heading()
         self.time_to_send_compact_data = 0
         self.last_execution_time = 0
 
@@ -213,15 +432,17 @@ class CanyonsOfMarsAgent:
 
     def connected(self):
         pyroslib.subscribe("canyons/command", self.handleAgentCommands)
-        pyroslib.subscribe("wheel/speed/status", self.handleOdo)
+        pyroslib.subscribe("wheel/speed/status", self.handleOdoSpeed)
+        pyroslib.subscribe("wheel/def/status", self.handleOdoOrientation)
         pyroslib.subscribe("sensor/distance", self.handleRadar)
+        pyroslib.subscribeBinary("sensor/heading/data", self.handleHeading)
         pyroslib.publish("canyons/feedback/action", self.current_action.getActionName())
         pyroslib.publish("canyons/feedback/running", self.running)
 
     def handleAgentCommands(self, topic, message, groups):
         data = message.split(" ")
 
-        log(DEBUG_LEVEL_INFO, "Got command " + message)
+        log(LOG_LEVEL_INFO, "Got command " + message)
 
         cmd = data[0]
         if cmd == "stop":
@@ -229,36 +450,18 @@ class CanyonsOfMarsAgent:
         elif cmd == "start":
             self.start(data[1:])
 
-    def handleOdo(self, topic, message, groups):
-        def deltaOdo(old, new):
-            d = new - old
-            if d > 32768:
-                d -= 32768
-            elif d < -32768:
-                d += 32768
+    def handleOdoSpeed(self, topic, message, groups):
+        self.odo.processSpeed(message.split(","))
 
-            return d
-
-        data = message.split(",")
-
-        for i in range(4):
-            data_index = i * 2
-            if data[data_index + 2] == "0":
-                newOdo = int(data[data_index + 1])
-                delta_odo = deltaOdo(self.last_odo[i], newOdo)
-                self.last_odo[i] = newOdo
-                self.odo[i] += delta_odo
+    def handleOdoOrientation(self, topic, message, groups):
+        self.odo.processOrientation(message.split(","))
 
     def handleRadar(self, topic, message, groups):
-        for d in self.radar:
-            self.last_radar[d] = self.radar[d]
+        self.radar.process([v.split(":") for v in message.split(" ")])
 
-        values = [v.split(":") for v in message.split(" ")]
-        for (k,v) in values:
-            if k == 'timestamp':
-                timestamp = float(v)
-            else:
-                self.radar[int(k)] = int(v)
+    def handleHeading(self, topic, message, groups):
+        heading_data = struct.unpack('>ffff', message)
+        self.heading.process(heading_data[2])
 
     def sendCompactData(self):
         pass
@@ -283,17 +486,20 @@ class CanyonsOfMarsAgent:
 
     def stop(self):
         self.running = False
+        self.heading.reset()
         self.nextAction(self.stop_action)
+        pyroslib.publish("position/heading/stop", '')
 
     def start(self, data):
         if not self.running:
+            pyroslib.publish("position/heading/start", '{"frequency":20}')
             pyroslib.publish("canyons/feedback/running", "True")
 
             self.running = True
 
             speed = int(data[0])
 
-            self.nextAction(MazeCorridorAction(self.radar, MazeCorridorAction.RIGHT, 300, speed))
+            self.nextAction(MazeCorridorAction(self.odo, self.radar, self.heading, MazeCorridorAction.RIGHT, 300, speed))
 
 
 if __name__ == "__main__":
--- client/canyons-of-mars/canyons-of-mars-client.py
+++ client/canyons-of-mars/canyons-of-mars-client.py
@@ -13,6 +13,7 @@ import pyros.gcc
 import pyros.gccui
 import pyros.agent
 import pyros.pygamehelper
+import struct
 import sys
 import time
 
@@ -35,7 +36,8 @@ class CanyonsOfMars:
         self.running = False
         self.radar = {0: 0, 45: 0, 90: 0, 135: 0, 180: 0, 225: 0, 270: 0, 315: 0}
         self.last_radar = {0: 0, 45: 0, 90: 0, 135: 0, 180: 0, 225: 0, 270: 0, 315: 0}
-        self.orientation = 0
+        self.heading = 0
+        self.start_heading = -1
         self.front_distance = 4000
         self.back_distance = 4000
         self.left_angle = 0
@@ -51,6 +53,7 @@ class CanyonsOfMars:
         self.right_front_distance_label = None
         self.front_distance_label = None
         self.back_distance_label = None
+        self.heading_label = None
 
     def connected(self):
         pass
@@ -61,15 +64,19 @@ class CanyonsOfMars:
         pyros.publish("canyons/command", "start " + str(60))
         self.onOffButton.on()
 
-    def stop(self):
+    def _stop(self):
         self.running = False
-        pyros.publish("canyons/command", "stop")
         self.onOffButton.off()
+        self.heading = 0
+        self.start_heading = -1
+
+    def stop(self):
+        self._stop()
+        pyros.publish("canyons/command", "stop")
 
     def handleRunning(self, topic, message, groups):
         if message == 'False':
-            self.running = False
-            self.onOffButton.off()
+            self._stop()
 
     def handleAction(self, topic, message, groups):
         self.onOffButton.label.setText(message)
@@ -109,6 +116,21 @@ class CanyonsOfMars:
             if k != 'timestamp':
                 self.radar[int(k)] = int(v)
 
+    def handleHeading(self, topic, message, groups):
+        heading_data = struct.unpack('>ffff', message)
+        orientation = heading_data[2]
+        if orientation < 0:
+            orientation += 360
+        if self.start_heading == -1:
+            self.start_heading = orientation
+        else:
+            orientation -= self.start_heading
+        if orientation < 0:
+            orientation += 360
+
+        self.heading = orientation
+        self.heading_label.setText("{:3.2f}".format(self.heading))
+
 
 canyonsOfMars = CanyonsOfMars()
 
@@ -120,7 +142,7 @@ class MazeCorridorComponent(gccui.components.CardsCollection):
         self.rover_image = pygame.image.load("rover-top-no-cover-48.png")
 
         self.radar = None
-        self.orientation = 0
+        self.heading = 0
         self.front_distance = 4000
         self.back_distance = 4000
         self.left_angle = 0
@@ -228,7 +250,129 @@ class MazeCorridorComponent(gccui.components.CardsCollection):
         drawWall(self.radar[270], self.radar[315], self.left_angle, -1, self.left_front_distance > 50)
         drawFrontWall(self.radar[0], (self.right_angle - self.left_angle) / 2, self.left_front_distance > 0, self.right_front_distance > 0)
 
-        rotated_rover_image = pygame.transform.rotate(self.rover_image, -self.orientation)
+        rotated_rover_image = pygame.transform.rotate(self.rover_image, -self.heading)
+        rotated_rover_image.get_rect(center=self.rect.center)
+        self.rover_image_component._surface = rotated_rover_image
+
+        super(MazeCorridorComponent, self).draw(surface)
+
+
+class MazeCornerComponent(gccui.components.CardsCollection):
+    def __init__(self, rect):
+        super(MazeCornerComponent, self).__init__(rect)
+
+        self.rover_image = pygame.image.load("rover-top-no-cover-48.png")
+
+        self.radar = None
+        self.heading = 0
+        self.front_distance = 4000
+        self.back_distance = 4000
+        self.left_angle = 0
+        self.right_angle = 0
+        self.left_front_distance = 0
+        self.right_front_distance = 0
+
+        self.scale = self.rect.width / 2
+
+        self.rover_image_component = uiFactory.image(self.rect.copy(), self.rover_image)
+        self.rover_image_component.rect.width = self.rover_image.get_width()
+        self.rover_image_component.rect.height = self.rover_image.get_height()
+        self.rover_image_component.rect.center = self.rect.center
+        self.addComponent(self.rover_image_component)
+
+    def draw(self, surface):
+        def drawRadar(deg):
+            d = self.radar[deg]
+
+            if deg == 90 or deg == 180 or deg == 270 or deg == 0:
+                if d > 1200:
+                    d = 1200
+            else:
+                if d > 1700:
+                    d = 1700
+
+            d = int(d * self.scale / 1200)
+            deg = deg * math.pi / 180 - math.pi / 2
+            x = int(math.cos(deg) * d + self.rect.center[0])
+            y = int(math.sin(deg) * d + self.rect.center[1])
+            pygame.draw.line(surface, pygame.color.THECOLORS['gray48'], self.rect.center, (x, y))
+            pygame.draw.circle(surface, pygame.color.THECOLORS['gray48'], (x, y), 3)
+
+        def drawWall(side_distance, front_distance, angle, mod, corner):
+            angle = angle / 2
+            side_distance = int(side_distance * self.scale / 1200)
+            front_distance = int(front_distance * self.scale / 1200)
+
+            x0 = int(mod * side_distance)
+            y0 = 0
+
+            x2 = int(mod * math.sin(angle * math.pi / 180) * self.scale + x0)
+            y2 = self.scale
+
+            if corner:
+                x1 = mod * side_distance
+                y1 = 0
+                if (x2 - x1) != 0.0:
+                    k = (y2 - y1) / (x2 - x1)
+                else:
+                    k = 1000000000.0
+                y = (y1 - k * x1) / (1 + mod * k)
+                x = y
+                x1 = int(- mod * x)
+                y1 = int(y)
+                x1 = x1 + self.rect.center[0]
+                y1 = y1 + self.rect.center[1]
+
+                x31 = mod * self.scale
+                y31 = -mod * x2
+
+                x31 = x31 + self.rect.center[0]
+                y31 = y31 + self.rect.center[1]
+
+                pygame.draw.line(surface, pygame.color.THECOLORS['green2'], (x1, y1), (x31, y31))
+
+            else:
+                x1 = int(-mod * math.sin(angle * math.pi / 180) * self.scale + x0)
+                y1 = -self.scale
+
+                x1 = x1 + self.rect.center[0]
+                y1 = y1 + self.rect.center[1]
+            x2 = x2 + self.rect.center[0]
+            y2 = y2 + self.rect.center[1]
+
+            pygame.draw.line(surface, pygame.color.THECOLORS['green2'], (x1, y1), (x2, y2))
+
+        def drawFrontWall(front_distance, angle, left_corner, right_corner):
+            if front_distance < 1200:
+                front_distance = int(front_distance * self.scale / 1200)
+
+                angle = - angle + math.pi / 2
+
+                x0 = 0
+                y0 = -front_distance
+
+                x1 = -self.scale
+                y1 = int(-math.sin(angle * math.pi / 180) * self.scale + y0)
+
+                x2 = self.scale
+                y2 = int(math.sin(angle * math.pi / 180) * self.scale + y0)
+
+                x1 = x1 + self.rect.center[0]
+                y1 = y1 + self.rect.center[1]
+                x2 = x2 + self.rect.center[0]
+                y2 = y2 + self.rect.center[1]
+
+                pygame.draw.line(surface, pygame.color.THECOLORS['green2'], (x1, y1), (x2, y2))
+
+        pygame.draw.rect(surface, pygame.color.THECOLORS['green'], self.rect, 1)
+        for d in [0, 45, 90, 135, 180, 225, 270, 315]:
+            drawRadar(d)
+
+        drawWall(self.radar[90], self.radar[45], self.right_angle, 1, self.right_front_distance > 50)
+        drawWall(self.radar[270], self.radar[315], self.left_angle, -1, self.left_front_distance > 50)
+        drawFrontWall(self.radar[0], (self.right_angle - self.left_angle) / 2, self.left_front_distance > 0, self.right_front_distance > 0)
+
+        rotated_rover_image = pygame.transform.rotate(self.rover_image, -self.heading)
         rotated_rover_image.get_rect(center=self.rect.center)
         self.rover_image_component._surface = rotated_rover_image
 
@@ -288,6 +432,7 @@ def initGraphics(screens):
     canyonsOfMars.right_front_distance_label = uiFactory.label(Rect(470, 50, 70, 20), "", h_alignment=gccui.ALIGNMENT.RIGHT)
     canyonsOfMars.front_distance_label = uiFactory.label(Rect(580, 30, 70, 20), "", h_alignment=gccui.ALIGNMENT.RIGHT)
     canyonsOfMars.back_distance_label = uiFactory.label(Rect(580, 50, 70, 20), "", h_alignment=gccui.ALIGNMENT.RIGHT)
+    canyonsOfMars.heading_label = uiFactory.label(Rect(630, 80, 70, 20), "", h_alignment=gccui.ALIGNMENT.RIGHT)
     statusComponents.addComponent(uiFactory.label(Rect(330, 30, 30, 20), "LA:"))
     statusComponents.addComponent(canyonsOfMars.left_angle_label)
     statusComponents.addComponent(uiFactory.label(Rect(440, 30, 30, 20), "RA:"))
@@ -300,6 +445,9 @@ def initGraphics(screens):
     statusComponents.addComponent(canyonsOfMars.front_distance_label)
     statusComponents.addComponent(uiFactory.label(Rect(550, 50, 30, 20), "BD:"))
     statusComponents.addComponent(canyonsOfMars.back_distance_label)
+
+    statusComponents.addComponent(uiFactory.label(Rect(630, 80, 30, 20), "H:"))
+    statusComponents.addComponent(canyonsOfMars.heading_label)
 
     maze_component = MazeCorridorComponent(Rect(10, 80, 600, 600))
     maze_component.radar = canyonsOfMars.radar
@@ -327,11 +475,12 @@ pyros.init("canyons-of-mars-#", unique=True, onConnected=connected, host=pyros.g
 pyros.subscribe("canyons/feedback/action", canyonsOfMars.handleAction)
 pyros.subscribe("canyons/feedback/running", canyonsOfMars.handleRunning)
 pyros.subscribe("canyons/feedback/corridor", canyonsOfMars.handleOrientaion)
+pyros.subscribeBinary("sensor/heading/data", canyonsOfMars.handleHeading)
 pyros.subscribe("sensor/distance", canyonsOfMars.handleDistances)
 
-uiFactory = gccui.BoxBlueSFTheme.BoxBlueSFThemeFactory()
-uiFactory.font = pyros.gccui.font
 uiAdapter = gccui.UIAdapter(screen)
+uiFactory = gccui.BoxBlueSFTheme.BoxBlueSFThemeFactory(uiAdapter)
+uiFactory.font = pyros.gccui.font
 
 screensComponent = gccui.CardsCollection(screen.get_rect())
 uiAdapter.setTopComponent(screensComponent)
