f62266dcaf143963263674a35e60bbe2675929ea
Daniel Sendula <natdan@users.noreply.github.com>
2019-03-03 15:59:32 +0000

    Updated position service

--- rover/type-c/position_service/position_service.py
+++ rover/type-c/position_service/position_service.py
@@ -13,8 +13,13 @@ import pyroslib
 import sys
 import os
 import time
+
+from decawave_1001_rjg import DwmLocationResponse
+from lsm9ds1_rjg import Driver, SPITransport
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
+from rover_position_rjg.data.data_pump.data_provider import DataProvider
 from rover_position_rjg.data.data_pump.process_data_pump import ProcessDataPump
 from rover_position_rjg.mqtt.mqtt_client import MqttClient
 from rover_position_rjg.position.calibration.decawave.decawave_range_scaler import DecawaveRangeScaler
@@ -22,7 +27,7 @@ from rover_position_rjg.position.calibration.decawave.two_way_ranging_calibratio
 from rover_position_rjg.sensors.decawave.decawave_data_provider import DecawaveDataProvider
 from rover_position_rjg.sensors.decawave.fixed_position_data_provider import FixedPositionDataProvider
 from rover_position_rjg.sensors.imu.imu_data_provider import ImuDataProvider
-from rover_position_rjg.services.position.position_service import PositionService
+from rover_position_rjg.services.position.position_service import PositionService, NineDoFData
 
 decawave_data_frequency = 10
 
@@ -36,6 +41,7 @@ def create_dummy_decawave_provider():
 # This service is providing positioning.
 #
 
+
 DEBUG = False
 
 
@@ -44,13 +50,21 @@ def stopCallback():
     position_serivce.quit()
 
 
+def create_imu_data_provider() -> DataProvider[NineDoFData]:
+    driver = Driver(
+        SPITransport(2, False, ImuDataProvider.PIN_INT1_AG),
+        SPITransport(0, True),
+        high_priority=True)
+    return ImuDataProvider(driver)
+
+
 if __name__ == "__main__":
     try:
         print("Starting position service...")
 
         pyroslib.init("echo-service", onStop=stopCallback)
 
-        print("Position started. (PID {})".format(os.getpid()))
+        print("  Position started. (PID {})".format(os.getpid()))
         imu_data_frequency = 230.8  # 119 or 238 or 476 are ODR for accelerometer and gyro
         start = time.time()
         local_g = 9.81255  # Wolston
@@ -64,7 +78,7 @@ if __name__ == "__main__":
             TwoWayRangingCalibration('DB08', -2, 75),
         ]
         decawave_range_scaler = DecawaveRangeScaler(6.4, anchor_calibrations)
-        ambient_temp = 10
+        ambient_temp = 20
 
         # Choose the calibration files
         dir_name = os.path.join(os.path.dirname(__file__), 'rover_1_35')
@@ -82,13 +96,18 @@ if __name__ == "__main__":
             decawave_calibration_filename,
             decawave_range_scaler,
             ambient_temp,
-            ProcessDataPump(ImuDataProvider, 1.5 / imu_data_frequency, 'IMU', samples_to_reject=15),
+            ProcessDataPump(create_imu_data_provider, 1.5 / imu_data_frequency, 'IMU', samples_to_reject=15),
             # ProcessDataPump(DecawaveDataProvider, 1.5/decawave_data_frequency, 'Decawave'),
-            ProcessDataPump(create_dummy_decawave_provider, 2 / decawave_data_frequency, 'Decawave'),
+            # ProcessDataPump(create_dummy_decawave_provider, 2/decawave_data_frequency, 'Decawave'),
+            None,
             MqttClient('position', 'position', 0.01)
-        )
-        threading.Thread(target=position_serivce.run, daemon=True).start()
-        print("Position finished in {:.1f} seconds".format(time.time() - start))
+        ).run()
+        print("  Position finished in {:.1f} seconds".format(time.time() - start))
+
+        time.sleep(5)
+
+        pyroslib.publish("position/pause", "")
+        print("  Paused position service.")
 
         print("Started position service.")
         pyroslib.forever(0.5)
