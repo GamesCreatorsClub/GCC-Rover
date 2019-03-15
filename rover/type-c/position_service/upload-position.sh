874c2bcf8af1aca972ce88e9218cfc65b0ef4c46
Daniel Sendula <natdan@users.noreply.github.com>
2019-03-15 17:35:35 +0000

    Fixed positioning service upload bug

--- rover/type-c/position_service/upload-position.sh
+++ rover/type-c/position_service/upload-position.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo ""
 echo Uploading        position
-pyros $1 upload -s -r -e python3.7 position $DIR/position_service.py -e $DIR/rover_1 -e $DIR/rover_1_35
+pyros $1 upload -s -r -e python3.7 position $DIR/position_service.py -e $DIR/rover_1 $DIR/rover_1_35
 echo Restarting       position
 pyros $1 restart      position
