67502234821514d003ea6a9fdda52fd912368cb8
Daniel Sendula <natdan@users.noreply.github.com>
2019-03-25 21:18:09 +0000

    Added uplink service

--- rover/uplink_service.py
+++ rover/uplink_service.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Copyright 2016-2017 Games Creators Club
+# Copyright 2016-2019 Games Creators Club
 #
 # MIT License
 #
@@ -10,38 +10,60 @@ import traceback
 import pyroslib
 
 #
-# echo service
+# uplink service
 #
-# This service is just sending echo back to different topic.
+# This service is just turning servo one way or another
 #
 
 DEBUG = False
 
 
-def handleEcho(topic, payload, groups):
-    print("Got echo in " + payload)
-    if len(groups) > 0:
-        pyroslib.publish("echo/out", groups[0] + ":" + payload)
-    else:
-        pyroslib.publish("echo/out", "default:" + payload)
+UPLINK_SERVO = 3
+MIN = 80
+MAX = 220
+
+position = MIN
+
+direction = 1
+
+
+def moveServo(servoid, position):
+    try:
+        with open("/dev/servoblaster", 'w') as f:
+            f.write(str(servoid) + "=" + str(position) + "\n")
+    except Exception as ex:
+        print("Sending servo exception: " + str(ex) + "\n" + ''.join(traceback.format_tb(ex.__traceback__)))
 
 
 def stopCallback():
     print("Asked to stop!")
 
 
+def loop():
+    global position, direction
+
+    position += direction
+
+    if position >= MAX:
+        direction = -direction
+
+    if position <= MIN:
+        direction = -direction
+
+    servo_position = int(position)
+    print("Moving servo to " + str(servo_position))
+    moveServo(UPLINK_SERVO, servo_position)
+
+
 if __name__ == "__main__":
     try:
-        print("Starting echo service...")
+        print("Starting uplink service...")
 
-        pyroslib.subscribe("echo/in", handleEcho)
-        pyroslib.subscribe("echo/in/a/b/#", handleEcho)
-        pyroslib.subscribe("echo/in/b/+/c", handleEcho)
-        pyroslib.init("echo-service", onStop=stopCallback)
+        pyroslib.init("uplink-service", onStop=stopCallback)
 
-        print("Started echo service.")
+        print("Started uplink service.")
 
-        pyroslib.forever(0.5)
+        pyroslib.forever(0.02, loop, priority=pyroslib.PRIORITY_LOW)
 
     except Exception as ex:
         print("ERROR: " + str(ex) + "\n" + ''.join(traceback.format_tb(ex.__traceback__)))
